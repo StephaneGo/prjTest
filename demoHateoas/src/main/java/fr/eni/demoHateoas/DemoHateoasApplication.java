0696b2b198a941db258ead7d82d75e9f9a55329d
Stephane Gobin <sgobin@campus-eni.fr>
2022-06-09 11:34:25 +0200

    premier commit

---
+++ demoHateoas/src/main/java/fr/eni/demoHateoas/DemoHateoasApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.demoHateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHateoasApplication.class, args);
+	}
+
+}
